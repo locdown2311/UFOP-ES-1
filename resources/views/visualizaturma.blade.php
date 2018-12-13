68e528ad15450093197fbfe51ebbb1b63e6c699a
Igor Carvalho Guimarães <igor2311@gmail.com>
2018-12-13 15:25:19 -0200

    Desativo opção de excluir turmas para evitar conflitos

--- resources/views/visualizaturma.blade.php
+++ resources/views/visualizaturma.blade.php
@@ -3,37 +3,47 @@
 @section('content')
 <div class="container">
     <div class="row">
-            <table class="table table-striped">
-                    <thead>
-                      <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Descrição</th>
-                        <th>Autor</th>
-                        <th colspan="2">Ação</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      
-                      @foreach($professores as $turma)
-                      <tr>
-                        <td>{{$turma['id']}}</td>
-                        <td>{{$turma['nome']}}</td>
-                        <td>{{$turma['descricao']}}</td>
-                        <td>{{$turma['autor']}}</td>
+      <div class="col-md-8 offset-md-2">
+          <div class="card">
+              <div class="card-header">
+                  Turmas presentes no sistema
+              </div>
+              <div class="card-body">
+                  <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th>ID</th>
+                          <th>Nome</th>
+                          <th>Descrição</th>
+                          <th>Autor</th>
+                          <th colspan="2">Ação</th>
+                        </tr>
+                      </thead>
+                      <tbody>
                         
-                        <td><a href="{{route('editarturma', ['id'=>$turma['id']])}}" class="btn btn-warning">Editar</a></td>
-                        <td>
-                          <form action="{{route('deletaturma', ['id'=>$turma['id']])}}" method="post">
-                            @csrf
-                            <input name="_method" type="hidden" value="DELETE">
-                            <button class="btn btn-danger" type="submit">Deletar</button>
-                          </form>
-                        </td>
-                      </tr>
-                      @endforeach
-                    </tbody>
-                  </table>
+                        @foreach($professores as $turma)
+                        <tr>
+                          <td>{{$turma['id']}}</td>
+                          <td>{{$turma['nome']}}</td>
+                          <td>{{$turma['descricao']}}</td>
+                          <td>{{$turma['autor']}}</td>
+                          
+                          <td><a href="{{route('editarturma', ['id'=>$turma['id']])}}" class="btn btn-warning">Editar</a></td>
+                          <td>
+                            <form action="{{route('deletaturma', ['id'=>$turma['id']])}}" method="post">
+                              @csrf
+                              <input name="_method" type="hidden" value="DELETE">
+                              <button class="btn btn-danger" disabled type="submit">Deletar</button>
+                            </form>
+                          </td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+              </div>
+          </div>
+      </div>
+            
     </div>
 </div>
 @endsection
